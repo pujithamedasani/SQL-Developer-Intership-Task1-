e2b08394995cffe3563c13b879248e2228bff9ac
Pujitha Medasani <mpujitha676@gmail.com>
2025-06-23 14:26:50 +0530

    CREATE TABLE
    I have created a table and inserted some data to the attributes and the output for the queries are attached

---
+++ queries.sql
@@ -0,0 +1,96 @@
+-- Staff table
+CREATE TABLE Staff (
+    StaffID INT PRIMARY KEY AUTO_INCREMENT,
+    Name VARCHAR(100) NOT NULL,
+    Role VARCHAR(50),
+    Email VARCHAR(100) UNIQUE
+);
+-- Insert into Staff
+INSERT INTO Staff (Name, Role, Email) VALUES
+(' Johnson', 'Librarian', 'johnson@gmail.com'),
+('ram', 'Assistant', 'ram@gmail.com');
+
+SELECT * FROM Staff;
+
+
+-- Members table
+CREATE TABLE Members (
+    MemberID INT PRIMARY KEY AUTO_INCREMENT,
+    Name VARCHAR(100) NOT NULL,
+    Email VARCHAR(100) UNIQUE,
+    JoinDate DATE
+);
+-- Insert into Members
+INSERT INTO Members (Name, Email, JoinDate) VALUES
+('Doe','doe@gmail.com', '2025-06-20'),
+('Jane','jane@gmail.com', '2025-03-12');
+
+SELECT * FROM Members;
+
+-- Books table
+CREATE TABLE Books (
+    BookID INT PRIMARY KEY AUTO_INCREMENT,
+    Title VARCHAR(200) NOT NULL,
+    ISBN VARCHAR(20) UNIQUE,
+    Status ENUM('Available', 'Loaned', 'Reserved') DEFAULT 'Available'
+);
+-- Insert into Books
+INSERT INTO Books (Title, ISBN, Status) VALUES
+('The Great Gatsby', '9780743273565', 'Available'),
+('1984', '9780451524935', 'Loaned'),
+(' A Good Girls Guide to Murder', '9780061120084', 'Reserved');
+
+SELECT * FROM Books;
+
+-- Reservations table
+CREATE TABLE Reservations (
+    ReservationID INT PRIMARY KEY AUTO_INCREMENT,
+    BookID INT,
+    MemberID INT,
+    ReservationDate DATE NOT NULL,
+    FOREIGN KEY (BookID) REFERENCES Books(BookID),
+    FOREIGN KEY (MemberID) REFERENCES Members(MemberID)
+);
+-- Insert into Reservations
+INSERT INTO Reservations (BookID, MemberID, ReservationDate) VALUES
+(1, 2, '2025-06-20');
+
+SELECT * FROM Reservations;
+
+-- Loans table
+CREATE TABLE Loans (
+    LoanID INT PRIMARY KEY AUTO_INCREMENT,
+    BookID INT,
+    MemberID INT,
+    StaffID INT,
+    LoanDate DATE NOT NULL,
+    DueDate DATE NOT NULL,
+    ReturnDate DATE,
+    FOREIGN KEY (BookID) REFERENCES Books(BookID),
+    FOREIGN KEY (MemberID) REFERENCES Members(MemberID),
+    FOREIGN KEY (StaffID) REFERENCES Staff(StaffID)
+);
+-- Insert into Loans
+INSERT INTO Loans (BookID, MemberID, StaffID, LoanDate, DueDate, ReturnDate) VALUES
+(2, 1, 1, '2025-06-01', '2025-06-15', NULL);
+
+
+SELECT * FROM Loans;
+
+-- Fines table
+CREATE TABLE Fines (
+    FineID INT PRIMARY KEY AUTO_INCREMENT,
+    LoanID INT,
+    StaffID INT,
+    Amount DECIMAL(6,2) NOT NULL,
+    IssuedDate DATE NOT NULL,
+    Paid BOOLEAN DEFAULT FALSE,
+    FOREIGN KEY (LoanID) REFERENCES Loans(LoanID),
+    FOREIGN KEY (StaffID) REFERENCES Staff(StaffID)
+);
+-- Insert into Fines
+INSERT INTO Fines (LoanID, StaffID, Amount, IssuedDate, Paid) VALUES
+(1, 2, 25.00, '2025-06-20', FALSE);
+
+
+SELECT * FROM Fines;
